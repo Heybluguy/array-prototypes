3b024eb2a7f34d6a108364f8e713f82d4d753724
Luis  Senior <heybluguy@yahoo.com>
2019-05-18 13:50:47 -0500

    removed comments

--- test/map-test.js
+++ test/map-test.js
@@ -1,25 +1,14 @@
 var assert = require('chai').assert;
-// var map = require('map');
 
 describe('Map', function () {
 
   it('should return an array of the length of each word', function () {
-    var words = ['float', 'chrysalis', 'butterfly', 'buzzing']; // Do not change this line
+    var words = ['float', 'chrysalis', 'butterfly', 'buzzing'];
+    var wordLengths = words.map(i => i = i.length );
 
-    var wordLengths = words.map(i => i = i.length ); // Write your code here to pass the assertions below
-
-    assert.deepEqual(words, ['float', 'chrysalis', 'butterfly', 'buzzing']);  // Do not change this line
-    assert.deepEqual(wordLengths, [5, 9, 9, 7]);  // Do not change this line
+    assert.deepEqual(words, ['float', 'chrysalis', 'butterfly', 'buzzing']);
+    assert.deepEqual(wordLengths, [5, 9, 9, 7]);
   });
 
 
 });
-
-// var assert = require('chai').assert;
-// var map = require('../map');
-
-// describe('Map', function () {
-//   it('should return hello', function () {
-//     assert.equal(map(), 'hello');
-//   })
-// })
